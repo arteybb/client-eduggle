9ccde6e1d76f73f96faf527dba7e14009a08edb8
bindzer <bindzertech.development@gmail.com>
2025-03-26 16:13:02 +0700

    test socket

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -165,7 +165,7 @@ interface Notification {
 const notifications = ref<Notification[]>([]);
 
 const initWebSocket = () => {
-  const socketUrl = `${import.meta.env.VITE_BASE_URL}/ws`;
+  const socketUrl = `${import.meta.env.VITE_BASE_URL}/ws/`;
   const socket = io(socketUrl, {
     query: { userId: authStore.user._id },
   });
